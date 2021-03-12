5571b6b393c2b98ad11c7e539167fc896614322a
Missy Lovegren <audrey.lovegren@utexas.edu>
2021-03-12 15:25:52 -0500

    Refactored offense and defense features

--- lesson_6/ttt_pedac.rb
+++ lesson_6/ttt_pedac.rb
@@ -56,51 +56,17 @@
 # After a few attempts, We took a peak at the LS solution and are going to approach the problem in a different way.
 
 # # Algorithm
-# - First, find at-risk squares within a given winning line that are valid moves
-#   - Conditions:
-#     - Square must be empty
-#     - The other two elements in the line are occupied by player markers, in the board hash.
-
-# Steps
-# - Create an empty array for at-risk squares
-#   - Count the elements in the line for which the value at the corresponding board hash key is a player marker
-#   - If the count equals two and the element corresponds to an empty string,
-#     - Add the element to the at-risk squares array
-
-# Because the method above can only work with a single line at a time, we need to call this method for every winning line until an at-risk square is found. 
-# - Define another method to find all risk squares on the board, passing in only the board as an argument
-#   - Create an array for the risk squares
-#   - Initialize a variable at 0 for the current winning line
-#   - Create a loop
-#     - Break if the array has any value or we've exhausted all winning lines
-#     - Add the result of finding the risk squares for the given line
-#     - Add 1 to counter
-# - If the risk square array is empty, return a random square in empty squares
-# - Otherwise, return the first (and only) value of risk_square
-
-# # LS Solution:
-# def find_at_risk_square(line, board) # define method and pass in a line from WINNING_LINES and the current board
-#   if board.values_at(*line).count('X') == 2 # checks how many times the board values for the elements in line equal 'X'. Evaluate next line if that is two.
-#     board.select{|k,v| line.include?(k) && v == ' '}.keys.first # Selects the key value pairs for which the key is present in the line, and the value is empty. Then returns an array of only the keys, then selects the first element in the array.
-#   else
-#     nil # Returns nil if one or fewer values in the board hash are 'X' at all elements in the line
-#   end
-# end
-
-# def computer_places_piece!(brd)
-#   square = nil # initialize square at nil
-#   WINNING_LINES.each do |line| # Iterate through winning lines, assigning the current element in the collection equal to the local variable `line`
-#     square = find_at_risk_square(line, brd) # Calls find_at_risk_squares passing in the current line and the board. Recall this returns nil if there are no at risk squares in the current line.
-#     break if square # break out of the loop if we found an at risk square in the current line. Otherwise, move on to the next line
-#   end
-
-#   if !square # If we ended up finding no at risk squares (square = nil)
-#     square = empty_squares(brd).sample # Make the square a random piece
-#   end
-
-#   brd[square] = COMPUTER_MARKER # Set hash value of board at square equal to the computer's piece
-# end
-
+# Define a method to which we'll pass in a winning line and the board
+# - Select the elements of the line for which the corresponding value of the board is equal to the player's marker. 
+# - If there are two such elements,
+#   - Return the third element in the line (that is, the one which corresponds to a blank value in the board hash)
+# Create a method to which we'll pass the board. This method will determine the computer's move, taking into consideration all winning lines
+# - Initialize a computer move variable equal to nil
+# - Iterate through winning lines
+#   - Call the method we defined above on the current line, and set the return equal to the computer's move
+#   - Break out of the iteration once computer's move has a truthy value (an Integer)
+# - Set the computer's move equal to a random one if another Integer was not selected
+# - Return the computer's move
 
 # # # 4. Computer AI: Offense
 # # Problem:  If the computer already has 2 in a row, then fill in the 3rd square, as opposed to moving at random.
@@ -120,8 +86,20 @@
 # # Data Structure
 # - Like the problem description says, we can parallel the logic in our find_at_risk_squares method to write a method that finds a winning square. When faced with the choice of defending or offending, we'll make the computer defend first since the player always takes the first move.
 
-# # Algorithm
-
+# # Algorithm (this will be very similar to our defense feature)
+# Define a method to which we'll pass in a winning line and the board
+# - Select the elements of the line for which the corresponding value of the board is equal to the computer's marker. 
+# - If there are two such elements,
+#   - Return the third element in the line (that is, the one which corresponds to a blank value in the board hash)
+# Add some funtionality to our method that determine's the computer's move
+# - If, after iterating through the winning lines array looking for defensive moves, the computer's move is still nil,
+#   - Iterate through winning lines
+#     - Call the method defined directly above on the current line
+#     - Break once the computer's move is truthy (an Integer)
+# - Set the computer's move equal to a random one if another Integer was not selected
+# - Return the computer's move
+# Within the computer places piece method, call the method defined directly above. Set this return value equal to the computer's square.
+# Set the value of the board hash at the key 'square' equal to the computer's marker.
 
 
 # {1=>" ", 2=>" ", 3=>" ", 4=>" ", 5=>" ", 6=>" ", 7=>" ", 8=>" ", 9=>" "} 
